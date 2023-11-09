adb82be95874c63d4890fd603680d4f4d1b58dbb
Raymond Ha <Haro@mail.uc.edu>
2023-11-09 02:46:36 -0500

    Final

--- src/DataStreamsRunner.java
+++ src/DataStreamsRunner.java
@@ -1,5 +1,6 @@
-public class Main {
+public class DataStreamsRunner {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        DataStreamsFrame streamsFrame = new DataStreamsFrame();
     }
 }
